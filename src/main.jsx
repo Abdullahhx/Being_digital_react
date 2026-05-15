75706826ab7d80a614e3bf531cde783efb07f4b6
taniaseed9 <saeedtania77@gmail.com>
2026-05-15 20:50:52 +0500

    Optimize scroll reveal animations: snappier transitions and earlier triggers

--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,8 @@ createRoot(document.getElementById('root')).render(
 
 // Simple Scroll Reveal Logic
 const observerOptions = {
-  threshold: 0.1,
-  rootMargin: '0px 0px -50px 0px'
+  threshold: 0.05,
+  rootMargin: '0px 0px -20px 0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
